47daaed1441e8734a6a1c99c958efe0dbfc05262
Igor Matnyak <igormatnak092@gmail.com>
2024-10-18 19:36:36 +0300

    creat first button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,3 +1,6 @@
+
+
+
 import { ButtonSvg } from '../svg/ButtonSvg'
 import './Button.scss'
 import React from 'react'
@@ -13,7 +16,7 @@ const Button: React.FC<ButtonProps> = ({label}) => {
 		<button
 			className='button'
 		>
-			<ButtonSvg className='button__icon' />
+			<ButtonSvg className='button__icon'/>
 			<div className='button__text'>{label}</div>
 		</button>
 	)
